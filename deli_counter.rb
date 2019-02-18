c211dfca00cfe286317b3717386f5c0f70ffa0f6
Shawn Ren <doggy-ren@hotmail.com>
2019-02-18 01:45:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,9 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
+    results = "The line is currently: "
     array.each_with_index do |name, index|
-      "The line is currently: #{index+1}. #{name}"
+      results += "#{index+1}. #{name}"
     end
   end
 end
