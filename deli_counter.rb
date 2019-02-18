c697a04e5647093ecc345b28e76b1e6d8a4f386e
Shawn Ren <doggy-ren@hotmail.com>
2019-02-18 01:33:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli=[]
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    newarray=[]
+    i=0
+    while i < array.length
+      newarray.push("#{i+1}. array[i]")
+      i += 1
+    end
+    puts "The line is currently: + #{newarray}"
+  end
+end
