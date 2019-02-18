891d19d2ed31f2bef30985ccbc48dac124c1ed34
Shawn Ren <doggy-ren@hotmail.com>
2019-02-18 01:54:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,12 @@ def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody wating to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
